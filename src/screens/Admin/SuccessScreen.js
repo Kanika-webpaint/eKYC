37ba22c88b46b4b6de8de4cdfbf9cb70d4fcc70f
Kanika-webpaint <kanika.webpainttech@gmail.com>
2024-02-16 16:23:07 +0530

    dummy invoice generated and download

--- src/screens/Admin/SuccessScreen.js
+++ src/screens/Admin/SuccessScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { View, TextInput, FlatList, Text, Image, StyleSheet, ScrollView, TouchableOpacity, Alert, Button, Linking } from 'react-native';
+import { View, TextInput, PermissionsAndroid, FlatList, Text, Image, StyleSheet, ScrollView, TouchableOpacity, Alert, Button, Linking, Modal, ToastAndroid } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { back, close, profile, success } from '../../common/images';
+import { back, close, download, profile, success } from '../../common/images';
 import { useNavigation } from '@react-navigation/native';
 import { getUsersListAction } from '../../redux/actions/user';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -11,10 +11,13 @@ import { collection } from 'firebase/firestore';
 import colors from '../../common/colors';
 import RedButton from '../../components/RedButton';
 import Status from '../../components/Status';
-const SuccessScreen = () => {
+import RNFS from 'react-native-fs';
+import Loader from '../../components/ActivityIndicator';
 
+const SuccessScreen = ({ route }) => {
     const navigation = useNavigation();
     const [isLoading, setIsLoading] = useState(false);
+    const [invoiceShow, showInvoiceDetail] = useState(false);
     const dispatch = useDispatch()
 
 
@@ -23,7 +26,58 @@ const SuccessScreen = () => {
     }
 
 
+    const onPressInvoice = () => {
+        showInvoiceDetail(true)
+    }
+    const showAlert = (message) => {
+        if (Platform.OS === 'android') {
+            ToastAndroid.show(message, ToastAndroid.SHORT);
+        } else {
+            AlertIOS.alert(message);
+        }
+    };
 
+
+    const downloadInvoice = async () => {
+        setIsLoading(true);
+
+        try {
+            // Request permission to read and write files
+            const granted = await PermissionsAndroid.requestMultiple([
+                PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+                PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+            ]);
+
+            if (
+                granted['android.permission.READ_EXTERNAL_STORAGE'] === PermissionsAndroid.RESULTS.GRANTED &&
+                granted['android.permission.WRITE_EXTERNAL_STORAGE'] === PermissionsAndroid.RESULTS.GRANTED
+            ) {
+                // Permissions granted, proceed with downloading invoice
+                const invoiceContent = {
+                    planPrice: route?.params?.invoiceDetail?.planPrice,
+                    cardNumber: route?.params?.invoiceDetail?.cardNumber || '',
+                    email: route?.params?.invoiceDetail?.email,
+                    name: route?.params?.invoiceDetail?.name,
+                    address: route?.params?.invoiceDetail?.address,
+                    city: route?.params?.invoiceDetail?.city,
+                    state: route?.params?.invoiceDetail?.state
+                };
+
+                const csvContent = Object.keys(invoiceContent).map(key => `${key},${invoiceContent[key]}`).join('\n');
+                const path = RNFS.DownloadDirectoryPath + '/invoice.txt';
+                await RNFS.writeFile(path, csvContent, 'utf8');
+                showAlert('Invoice Downloaded successfully!!');
+            } else {
+                // Permissions not granted, handle accordingly (e.g., show an error message)
+                showAlert('Permissions denied. Unable to download invoice.');
+            }
+        } catch (error) {
+            console.error('Error generating invoice: ', error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+    
     return (
         <SafeAreaView style={styles.safeArea}>
             <Status isLight />
@@ -33,6 +87,80 @@ const SuccessScreen = () => {
                     <Text style={styles.pay}>Payment Successful!</Text>
                     <Text style={styles.confirmText}>The payment of N1000 has successfully been done.</Text>
                     <RedButton buttonContainerStyle={styles.buttonContainer} ButtonContent={isLoading ? <Loader /> : 'CONTINUE'} contentStyle={styles.buttonText} onPress={() => onPressContinue()} />
+                    <RedButton buttonContainerStyle={styles.buttonContainer} ButtonContent={isLoading ? <Loader /> : 'INVOICE'} contentStyle={styles.buttonText} onPress={() => onPressInvoice()} />
+                </View>
+                <View style={styles.container}>
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={invoiceShow}
+                        onRequestClose={() => {
+                            showInvoiceDetail(false);
+                        }}
+                    >
+                        <View style={styles.modalContainer}>
+                            <View style={styles.modalContent}>
+                                <Text style={styles.titleHead}>
+                                    Invoice Detail
+                                </Text>
+                                <TouchableOpacity style={styles.closeImg} onPress={() => showInvoiceDetail(false)}>
+                                    <Image style={{ height: 20, width: 20 }} source={close} />
+                                </TouchableOpacity>
+                                <View style={styles.viewMain}>
+                                    <Text style={styles.amountPaid}>Amount</Text>
+                                    <Text style={styles.pay}>{route?.params?.invoiceDetail?.planPrice || ''}</Text>
+                                    <View style={styles.itemView}>
+                                        <View style={{ flex: 0.5 }}>
+                                            <Text>Name</Text>
+                                            <Text>{route?.params?.invoiceDetail?.name || ''}</Text>
+                                        </View>
+                                        <View style={styles.viewItem}>
+                                            <Text>email</Text>
+                                            <Text>{route?.params?.invoiceDetail?.email || ''}</Text>
+                                        </View>
+                                    </View>
+                                    <View style={styles.midView}>
+                                        <View style={{ flex: 0.5 }}>
+                                            <Text>Card Number</Text>
+                                            <Text>{route?.params?.invoiceDetail?.cardNumber || ''}</Text>
+                                        </View>
+                                        <View style={styles.viewItem}>
+                                            <Text>Expiry Date</Text>
+                                            <Text>{route?.params?.invoiceDetail?.expiryDate || ''}</Text>
+                                        </View>
+                                    </View>
+                                    <View style={styles.midView}>
+                                        <View style={{ flex: 0.5 }}>
+                                            <Text>Address</Text>
+                                            <Text>{route?.params?.invoiceDetail?.address || ''}</Text>
+                                        </View>
+                                        <View style={styles.viewItem}>
+                                            <Text>Country</Text>
+                                            <Text>{route?.params?.invoiceDetail?.country || ''}</Text>
+                                        </View>
+                                    </View>
+                                    <View style={styles.midView}>
+                                        <View style={{ flex: 0.5 }}>
+                                            <Text>State</Text>
+                                            <Text>{route?.params?.invoiceDetail?.state || ''}</Text>
+                                        </View>
+                                        <View style={styles.viewItem}>
+                                            <Text>City</Text>
+                                            <Text>{route?.params?.invoiceDetail?.city || ''}</Text>
+                                        </View>
+                                    </View>
+                                    <TouchableOpacity style={styles.buttonContain} onPress={() => downloadInvoice()}>
+                                        {isLoading ? <Loader /> :
+                                            <>
+                                                <Image source={download} style={{ height: 20, width: 20, marginRight: 10 }}></Image>
+                                                <Text style={styles.buttonText}>{'INVOICE'}</Text>
+                                            </>}
+
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </View>
+                    </Modal>
                 </View>
             </ScrollView>
         </SafeAreaView>
@@ -44,6 +172,52 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: colors.light_purple,
         justifyContent: 'center',
+    },
+    titleHead: {
+        fontFamily: fonts.bold,
+        color: colors.grey,
+        fontSize: 20,
+        marginTop: 20,
+        marginLeft: 20
+    },
+    closeImg: {
+        position: 'absolute',
+        top: 20,
+        right: 20
+    },
+    viewMain: {
+        backgroundColor: colors.light_purple,
+        height: 100,
+        margin: 10,
+        marginTop: 30
+    },
+    amountPaid: {
+        alignSelf: 'center',
+        margin: 5,
+        fontFamily: fonts.bold,
+        fontSize: 18
+    },
+    pay: {
+        alignSelf: 'center',
+        margin: 10,
+        fontFamily: fonts.bold,
+        fontSize: 25,
+        color: colors.black
+    },
+    itemView: {
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 20
+    },
+    midView: {
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    viewItem: {
+        marginLeft: 20,
+        flex: 0.5
     },
     succesImg: {
         height: 150,
@@ -72,6 +246,17 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         width: '70%',
     },
+    buttonContain: {
+        marginTop: '10%',
+        flexDirection: 'row',
+        backgroundColor: colors.app_red,
+        paddingVertical: 10,
+        borderRadius: 8,
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '70%',
+    },
     buttonText: {
         color: colors.white,
         fontSize: 16,
@@ -84,7 +269,22 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         marginTop: '30%'
-    }
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalContent: {
+        backgroundColor: 'white',
+        height: '80%',
+        width: '90%',
+        borderRadius: 10,
+        elevation: 5,
+    },
+    container: {
+        flex: 1,
+    },
 });
 
 export default SuccessScreen;
